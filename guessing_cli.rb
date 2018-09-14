e0de851190b05939b77d8fc509e69c35e0802077
Nico-ehs <nico.ehs05@gmail.com>
2018-09-14 17:22:38 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -18,53 +18,53 @@ def exit_game
   puts "Goodbye!"
 end
 
-def help
-  puts """I accept the following commands:
-- help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
-- exit : exits this program"""
-end
+# def help
+#   puts """I accept the following commands:
+# - help : displays this help message
+# - list : displays a list of songs you can play
+# - play : lets you choose a song to play
+# - exit : exits this program"""
+# end
 
-def list(songs)
-  songs.each_with_index do |el, i|
-    puts "#{i+1}. #{el}"
-  end
-end
+# def list(songs)
+#   songs.each_with_index do |el, i|
+#     puts "#{i+1}. #{el}"
+#   end
+# end
 
-def play(songs)
-  puts "Please enter a song name or number:"
-  input = gets.chomp
-  if songs.include?(input)
-    puts "Playing #{input}"
-  elsif input >= "1" and input <= "9"
-    puts "Playing #{songs[input.to_i-1]}"
-  else
+# def play(songs)
+#   puts "Please enter a song name or number:"
+#   input = gets.chomp
+#   if songs.include?(input)
+#     puts "Playing #{input}"
+#   elsif input >= "1" and input <= "9"
+#     puts "Playing #{songs[input.to_i-1]}"
+#   else
     
-  end
+#   end
   
-  puts "Playing #{}"
-  puts "Invalid input, please try again"
-end
+#   puts "Playing #{}"
+#   puts "Invalid input, please try again"
+# end
 
-def exit_jukebox
-  puts "Goodbye"
-end
+# def exit_jukebox
+#   puts "Goodbye"
+# end
 
-def run(songs)
-  help
-  puts "Please enter a command:"
-  input = gets.chomp
-  return exit_jukebox if input = "exit"
-  play(songs) if input = "play"
-  list(songs) if input = "list"
-  help if input = "help"
-  run
-end
+# def run(songs)
+#   help
+#   puts "Please enter a command:"
+#   input = gets.chomp
+#   return exit_jukebox if input = "exit"
+#   play(songs) if input = "play"
+#   list(songs) if input = "list"
+#   help if input = "help"
+#   run
+# end
 
-def say_hello(name)
-  "Hi #{name}!"
-end
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
  
 # puts "Enter your name:"
 # users_name = gets.chomp
